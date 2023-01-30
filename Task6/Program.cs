91c78a9cea6d0dead8052bcb291206b7561f49ae
Aslan <at_aslan_07@mail.ru>
2023-01-30 17:13:27 +0300

    Task 6 четное/нечетное

---
+++ Task6/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+
+Console.Clear();
+Console.Write("Enter the number: ");
+double a = double.Parse(Console.ReadLine());
+double even = (a*a)/2;
+if (even % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
+
