a28a2088147f32cd98bb965cbb88fd13f7c13ba1
Aslan <at_aslan_07@mail.ru>
2023-02-04 13:47:43 +0300

    Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает вторую цифру этого
+// числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.Write("enter the three-digit number: ");
+int num = int.Parse(Console.ReadLine());
+if (num>99 && num <1000)
+{
+    int a1 = num/10;
+    int a2 = a1%10;
+        Console.Write($"{a2}");
+}
+
+else
+{
+    Console.Write("the entered number is not a three-digit number");
+}
